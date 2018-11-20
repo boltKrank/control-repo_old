964cb057eee5c7fbf4e404fc6e281286bd5ed2aa
boltKrank <simon.anderson111@gmail.com>
2018-11-20 17:46:54 +1100

    Update apache_terraform.pp

--- site/profile/manifests/apache_terraform.pp
+++ site/profile/manifests/apache_terraform.pp
@@ -1,6 +1,11 @@
 class profile::apache_terraform {
   
   class { 'apache':
+    default_vhost => false,
+  }
+  
+  apache::vhost { 'simon.example.com':
+    port => '80',
     docroot => '/tmp',
   }
 
